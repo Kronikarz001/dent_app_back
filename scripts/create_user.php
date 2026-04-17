377075ce06fd32e51013dc54d9213fd570ace1a2
Kronikarz001 <piotrek.11201@gmail.com>
2026-04-17 14:20:28 +0200

    ADD: make for create admin user in database

---
+++ scripts/create_user.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Console\Kernel;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$kernel->bootstrap();
+
+$user = new User();
+$user->first_name = getenv('CREATE_FIRST_NAME');
+$user->last_name = getenv('CREATE_LAST_NAME');
+$user->email = getenv('CREATE_EMAIL');
+$user->password = Hash::make(getenv('CREATE_PASSWORD'));
+$user->pesel = getenv('CREATE_PESEL');
+$user->is_admin = getenv('CREATE_IS_ADMIN');
+
+if (User::where('email', $user->email)->exists() || User::where('pesel', $user->pesel)->exists()) {
+    echo "User with this credentials already exists.\n";
+    exit;
+}
+
+$user->save();
+
+echo "User created successfully.\n";
