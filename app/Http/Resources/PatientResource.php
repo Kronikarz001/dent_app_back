0a8cc09c1fd3a4fc887c8349296056bbfc04a269
Kronikarz001 <piotrek.11201@gmail.com>
2026-04-24 14:37:46 +0200

    ADD: phone numbers to resource

--- app/Http/Resources/PatientResource.php
+++ app/Http/Resources/PatientResource.php
@@ -7,5 +7,14 @@ namespace App\Http\Resources;
  */
 class PatientResource extends BasicResource
 {
-
+    /**
+     * @param $request
+     * @return array
+     */
+    public function toArray($request): array
+    {
+        return array_merge(parent::toArray($request), [
+            'phone_number' => $this->phone_numbers,
+        ]);
+    }
 }
