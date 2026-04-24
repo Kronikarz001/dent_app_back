2663971527cdacadec20e081ab4074a05ebf2fa0
Kronikarz001 <piotrek.11201@gmail.com>
2026-04-24 14:44:15 +0200

    ADD: phone numbers to resource

--- app/Http/Resources/JobPositionResource.php
+++ app/Http/Resources/JobPositionResource.php
@@ -7,14 +7,5 @@ namespace App\Http\Resources;
  */
 class JobPositionResource extends BasicResource
 {
-    /**
-     * @param $request
-     * @return array
-     */
-    public function toArray($request): array
-    {
-        return array_merge(parent::toArray($request), [
-            'phone_number' => PhoneNumberResource::collection($this->phoneNumbers)
-        ]);
-    }
+
 }
